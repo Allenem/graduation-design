a1a25eaf272f08a622658eb826645b2dd44bff49
Allenem <1820988819@qq.com>
2020-05-03 12:48:26 +0800

    统一SURF特征点为15个，并按照半径排序

--- SVM/ExtractFeatureData/CommonFunction/extract_SURF_data.py
+++ SVM/ExtractFeatureData/CommonFunction/extract_SURF_data.py
@@ -12,6 +12,15 @@ def extract_SURF_data(inputpath, outputpath):
         # print(kp.pt[0], kp.pt[1], kp.angle, kp.size)
         kps_data.append([kp.pt[0], kp.pt[1], kp.angle, kp.size])
 
+    # 统一特征点为15个
+    kps_data_len = len(kps_data)
+    if kps_data_len < 15:
+        for i in range(15-kps_data_len):
+            kps_data.append([0, 0, 0, 0])
+    elif kps_data_len > 15:
+        del kps_data[15:]
+    # 按照半径排序
+    kps_data.sort(key=lambda x: x[3], reverse=True)
     titles = ['x', 'y', 'angle', 'diameter']
     dt = pd.DataFrame(kps_data, columns=titles)
     sheet_name = inputpath.split('/')[-1][:-4]
